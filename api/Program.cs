a324fd327f25c8b6a19f5813353b16cf4301a5d5
Kipras Lisauskas <kipraslisauskas@gmail.com>
2025-11-15 01:31:26 +0200

    Add users table seeding for testing purposes

--- api/Program.cs
+++ api/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using api.Models;
 using api.Data;
+using Microsoft.Extensions.Options;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,9 +12,49 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<ApiContext>(options => 
-    options.UseSqlite("Data Source=dbase.db"));
+    options.UseSqlite("Data Source=dbase.db")
+    .UseSeeding((context, _ ) =>
+    {
+        var users = context.Set<User>();
+        if (!users.Any())
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                users.Add(new User
+                {
+                   Name =  $"user{i}",
+                   Password = $"pass{i}",
+                   Email = $"mail{i}@gmail.com"
+                });
+            }
+            context.SaveChanges();
+        }
+    })
+    .UseAsyncSeeding(async (context, _, cancelToken) =>
+    {
+        var users = context.Set<User>();
+        if (!await users.AnyAsync())
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                users.Add(new User
+                {
+                   Name =  $"user{i}",
+                   Password = $"pass{i}",
+                   Email = $"mail{i}@gmail.com"
+                });
+            }
+            await context.SaveChangesAsync(cancelToken);
+        }
+    }));
 
 var app = builder.Build();
+
+await using (var serviceScope = app.Services.CreateAsyncScope())
+using (var dbContext = serviceScope.ServiceProvider.GetRequiredService<ApiContext>())
+{
+    await dbContext.Database.EnsureCreatedAsync();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5bc8eda2a552ad1614cea50454ee19013c6a89d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
